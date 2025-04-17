f796a9760dea8b00cbad7b749a66e99a1fee04f9
TAiGA <>
2025-04-17 17:53:06 +0800

    Add Serialize for XML

--- ui/minamoto/Markup.cpp
+++ ui/minamoto/Markup.cpp
@@ -8,39 +8,17 @@
 #include "miMarkup.h"
 #include "Markup.h"
 
-static std::string input = R"({
-  "first_name": "John",
-  "last_name": "Smith",
-  "is_alive": true,
-  "age": 27,
-  "address": {
-    "street_address": "21 2nd Street",
-    "city": "New York",
-    "state": "NY",
-    "postal_code": "10021-3100"
-  },
-  "phone_numbers": [
-    {
-      "type": "home",
-      "number": "212 555-1234"
-    },
-    {
-      "type": "office",
-      "number": "646 555-4567"
-    }
-  ],
-  "children": [
-    "Catherine",
-    "Thomas",
-    "Trevor"
-  ],
-  "spouse": null
-})";
+extern std::string inputJSON;
+extern std::string inputTOML;
+extern std::string inputXML;
+extern std::string inputYAML;
+static std::string input;
 static std::string output;
 static miMarkup* markup = nullptr;
 //------------------------------------------------------------------------------
 void Markup::Initialize()
 {
+    input = inputXML;
     markup = nullptr;
 }
 //------------------------------------------------------------------------------
@@ -112,15 +90,18 @@ bool Markup::Update(const UpdateData& updateData, bool& show)
     ImGui::SetNextWindowSize(ImVec2(1280.0f, 892.0f), ImGuiCond_Appearing);
     if (ImGui::Begin("Markup", &show, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoDocking))
     {
-        static int inputType = 'JSON';
+        static int inputType = 'XML ';
         static int outputType = 'XML ';
-        bool build = false;
+        static bool build = true;
 
         float width = ImGui::GetWindowWidth() / 2.0f - 16.0f;
         float height = ImGui::GetTextLineHeightWithSpacing() * 24;
 
         ImGui::Columns(2);
-        if (ImGui::RadioButton("JSON" Q, inputType == 'JSON')) { inputType = 'JSON'; build = true; } ImGui::SameLine();
+        if (ImGui::RadioButton("JSON" Q, inputType == 'JSON')) { input = inputJSON; inputType = 'JSON'; build = true; } ImGui::SameLine();
+        if (ImGui::RadioButton("TOML" Q, inputType == 'TOML')) { input = inputTOML; inputType = 'TOML'; build = true; } ImGui::SameLine();
+        if (ImGui::RadioButton("XML" Q, inputType == 'XML ')) { input = inputXML; inputType = 'XML '; build = true; } ImGui::SameLine();
+        if (ImGui::RadioButton("YAML" Q, inputType == 'YAML')) { input = inputYAML; inputType = 'YAML'; build = true; } ImGui::SameLine();
         ImGui::NewLine();
         if (ImGui::InputTextMultiline("" Q, input, ImVec2(width, height)))
             build = true;
@@ -145,16 +126,27 @@ bool Markup::Update(const UpdateData& updateData, bool& show)
 
         if (build)
         {
+            build = false;
             delete markup;
             switch (inputType)
             {
             case 'JSON':
                 markup = miMarkup::fromJSON(input);
                 break;
+            case 'TOML':
+                markup = miMarkup::fromTOML(input);
+                break;
+            case 'XML ':
+                markup = miMarkup::fromXML(input);
+                break;
+            case 'YAML':
+                markup = miMarkup::fromYAML(input);
+                break;
             default:
                 markup = nullptr;
                 break;
             }
+            output.clear();
             if (markup)
             {
                 switch (outputType)
@@ -172,7 +164,6 @@ bool Markup::Update(const UpdateData& updateData, bool& show)
                     output = (*markup).toYAML();
                     break;
                 default:
-                    output.clear();
                     break;
                 }
             }
@@ -182,4 +173,64 @@ bool Markup::Update(const UpdateData& updateData, bool& show)
 
     return updated;
 }
+//------------------------------------------------------------------------------
+std::string inputJSON = R"({
+  "first_name": "John",
+  "last_name": "Smith",
+  "is_alive": true,
+  "age": 27,
+  "address": {
+    "street_address": "21 2nd Street",
+    "city": "New York",
+    "state": "NY",
+    "postal_code": "10021-3100"
+  },
+  "phone_numbers": [
+    {
+      "type": "home",
+      "number": "212 555-1234"
+    },
+    {
+      "type": "office",
+      "number": "646 555-4567"
+    }
+  ],
+  "children": [
+    "Catherine",
+    "Thomas",
+    "Trevor"
+  ],
+  "spouse": null
+})";
+//------------------------------------------------------------------------------
+std::string inputTOML;
+//------------------------------------------------------------------------------
+std::string inputXML = R"(<first_name>John</first_name>
+<last_name>Smith</last_name>
+<is_alive>true</is_alive>
+<age>27</age>
+<address>
+ <street_address>21 2nd Street</street_address>
+ <city>New York</city>
+ <state>NY</state>
+ <postal_code>10021-3100</postal_code>
+</address>
+<phone_numbers>
+ <array>
+  <type>home</type>
+  <number>212 555-1234</number>
+ </array>
+ <array>
+  <type>office</type>
+  <number>646 555-4567</number>
+ </array>
+</phone_numbers>
+<children>
+ <array>Catherine</array>
+ <array>Thomas</array>
+ <array>Trevor</array>
+</children>
+<spouse>null</spouse>)";
+//------------------------------------------------------------------------------
+std::string inputYAML;
 //------------------------------------------------------------------------------
